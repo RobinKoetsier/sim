61e474a852cdfd9f4f3d4932a31b7a8625122792
Robin Koetsier <45625446+RobinKoetsier@users.noreply.github.com>
2023-06-23 20:07:34 +0200

    files!

--- create_data.py
+++ create_data.py
@@ -1,8 +1,13 @@
 import pandas as pd
 
 # Read the Parquet file
-df = pd.read_parquet('input.parquet')
-
+df = pd.read_parquet('Winger.parquet')
+df = pd.concat(pd.read_parquet('CAM.parquet'),
+        pd.read_parquet('CM.parquet'),
+        pd.read_parquet('DM.parquet'),
+        pd.read_parquet('FB.parquet'),
+        pd.read_parquet('ST.parquet'),
+        pd.read_parquet('Winger.parquet'))
 # Convert the DataFrame to CSV
 #df.to_csv('output.csv', index=False)
 
@@ -11,11 +16,11 @@ df = pd.read_parquet('input.parquet')
 
 # Get unique values in the 'Player 1' column
 unique_values = df['Player 1'].unique()
-
+len(unique_values)
 # Split the CSV into different files based on unique values
 for value in unique_values:
     filtered_df = df[df['Player 1'] == value]
-    filename = f'Players/{value}.csv'
+    filename = f'Players/{value.replace("/","")}.csv'
     filtered_df.to_csv(filename, index=False)
 
 print('CSV files split successfully.')
