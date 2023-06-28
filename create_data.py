8b36b307b622a2c4760679a1090bc95958975759
Robin Koetsier <45625446+RobinKoetsier@users.noreply.github.com>
2023-06-28 10:59:18 +0200

    player files

--- create_data.py
+++ create_data.py
@@ -1,13 +1,13 @@
 import pandas as pd
-
+import sys
 # Read the Parquet file
-df = pd.read_parquet('Winger.parquet')
-df = pd.concat([pd.read_parquet('CAM.parquet'),
-        pd.read_parquet('CM.parquet'),
-        pd.read_parquet('DM.parquet'),
-        pd.read_parquet('FB.parquet'),
-        pd.read_parquet('ST.parquet'),
-        pd.read_parquet('Winger.parquet')])
+df = pd.read_parquet('parquetfiles/CAM.parquet')
+df = pd.concat([pd.read_parquet('parquetfiles/CAM.parquet'),
+        pd.read_parquet('parquetfiles/CM.parquet'),
+        pd.read_parquet('parquetfiles/DM.parquet'),
+        pd.read_parquet('parquetfiles/FB.parquet'),
+        pd.read_parquet('parquetfiles/ST.parquet'),
+        pd.read_parquet('parquetfiles/Winger.parquet')])
 # Convert the DataFrame to CSV
 #df.to_csv('output.csv', index=False)
 
@@ -15,14 +15,30 @@ df = pd.concat([pd.read_parquet('CAM.parquet'),
 #df_csv = pd.read_csv('output.csv')
 
 # Get unique values in the 'Player 1' column
-unique_values = df['Player 1'].unique()
-len(unique_values)
+# unique_values = df['Player 1'].unique()
+# len(unique_values)
 # Split the CSV into different files based on unique values
-for value in unique_values:
-    filtered_df = df[df['Player 1'] == value]
-    filename = f'Players/{value.replace("/","")}.csv'
-    filtered_df.to_csv(filename, index=False)
+positions = ['CAM', 'CM', 'DM','FB','ST','Winger']
+positions = ['FB','ST','Winger']
+# positions = ['CAM']
+for pos in positions:
+    print(pos)
+    df = pd.read_parquet(f'parquetfiles/{pos}.parquet')
+    unique_values = df['Player 1'].unique()
+    for player in unique_values:
+        print(player)
+        base = df[(df['Player 1']==player) | (df['Player 2']==player)].sort_values(by=['Similarity'],ascending=False).reset_index(drop=True)
+        base['Player'] = (base['Player 2']).where(base['Player 1'] == player, base['Player 1'])
+        base = base.loc[:, ['Player 1', 'Similarity']]
+        filtered_df = base[base.Similarity >= base.Similarity.quantile(.98)].reset_index(drop=True)
+    # filtered_df = df[(df['Player 1'] == value | df['Player 2'] == value)]
+        filename = f'Players/{player.replace("/","")}.csv'
+        filtered_df.head(25).to_csv(filename, index=False)
 
+
+
+
+player = "D. Zabala (31, Nacional, Uruguay Primera División 2023)"
 print('CSV files split successfully.')
 
 csv_file = pd.DataFrame(unique_values, columns= list('a'))
--- scripts/script.js
+++ scripts/script.js
@@ -147,13 +147,14 @@ svg.append("g")
   .style("font-size", "10px") // Set the font size to 10 pixels
   .style("cursor", "pointer") // Add cursor pointer to the y-axis labels
   .on("click", function(_, d) { // Modify event binding to access the correct value
-    var labelValue = String(d).slice(0, String(d).indexOf("(")).trim(); // Convert d to a string and extract the value before "("
-    //var formattedValue = labelValue.replace(/\s/g, ""); // Remove spaces from the extracted value
-    var formattedValue = labelValue.substring(labelValue.lastIndexOf('.') + 1); 
-    console.log(formattedValue)
-    var transfermarktUrl = "https://www.transfermarkt.com/schnellsuche/ergebnis/schnellsuche?query=" + encodeURIComponent(formattedValue); // Construct the URL
+    //var labelValue = String(d).slice(0, String(d).indexOf("(")).trim(); // Convert d to a string and extract the value before "("
+    
+    //var formattedValue = labelValue.substring(labelValue.lastIndexOf('.') + 1); 
+    //console.log(formattedValue)
+    //var transfermarktUrl = "https://www.transfermarkt.com/schnellsuche/ergebnis/schnellsuche?query=" + encodeURIComponent(formattedValue); // Construct the URL
     //window.location.href = transfermarktUrl; // Redirect to the specified URL
-    window.open(transfermarktUrl, "_blank"); // Open the specified URL in a new tab
+    //window.open(transfermarktUrl, "_blank"); // Open the specified URL in a new tab
+    $("#dropdown").val(d).trigger("change");
   });
 
 
